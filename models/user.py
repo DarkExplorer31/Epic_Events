e71f36c1f9c61efd6503898ca731ac30f8eee58c
DarkExplorer31 <drkexplorer@gmal.com>
2024-02-13 15:06:08 +0100

    Add SQLAlchemy and define user model and authentication controller to create a authenticate method

--- models/user.py
+++ models/user.py
@@ -1,23 +1,13 @@
 """User model"""
 
+from datetime import datetime
 from sqlalchemy import Column, Integer, String, Date
-from sqlalchemy.sql import func
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-ROLE = ["saleperson", "event manager", "support"]
-
 
 class User(Base):
-    def __init__(self, role, complete_name, email, phone_number):
-        if role not in ROLE:
-            raise ValueError("Invalid role")
-        self.role = role
-        self.complete_name = complete_name
-        self.email = email
-        self.phone_number = phone_number
-
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -25,4 +15,5 @@ class User(Base):
     complete_name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
     phone_number = Column(String, unique=True, nullable=False)
-    creation_date = Column(Date, default=func.now(), nullable=False)
+    password = Column(String, nullable=False)
+    creation_date = Column(Date, default=datetime.now, nullable=False)
